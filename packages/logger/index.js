9c8a88037e5035ff3c542b532c0317c00d987794
Eric Sández <esandez93@gmail.com>
2019-05-21 23:37:48 +0200

    Moved to Lerna monorepo structure

---
+++ packages/logger/index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+const log = (color, ...args) => console.log(color(...args));
+
+exports.info = (...args) => log(chalk.italic.blue, ...args);
+exports.warn = (...args) => log(chalk.yellow, ...args);
+exports.error = (...args) => log(chalk.bold.red, ...args);
