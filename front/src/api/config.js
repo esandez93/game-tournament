8c0e2e5076fda278894bee7d101751ad54c209ef
Eric Sández <esandez93@gmail.com>
2019-05-27 01:23:20 +0200

    Deleted ending slash in API URL

--- front/src/api/config.js
+++ front/src/api/config.js
@@ -1,4 +1,4 @@
-const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3020' : 'https://game-tournament.herokuapp.com/';
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3020' : 'https://game-tournament.herokuapp.com';
 
 export {
   url
