2e8ea33a1be1ba5a19fd733786f8b05f79bfb04e
Eric Sández <esandez93@gmail.com>
2019-05-26 19:48:46 +0200

    Disabled Reactotron

--- front/src/ReactotronConfig.js
+++ front/src/ReactotronConfig.js
@@ -1,5 +1,9 @@
 import Reactotron from 'reactotron-react-js';
 
-Reactotron
-  .configure() // we can use plugins here -- more on this later
-  .connect(); // let's connect!
+/*
+if (process.env.NODE_ENV === 'development') {
+  Reactotron
+    .configure() // we can use plugins here -- more on this later
+    .connect(); // let's connect!
+}
+ */
--- front/src/serviceWorker.js
+++ front/src/serviceWorker.js
@@ -21,7 +21,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (/*process.env.NODE_ENV === 'production' && */'serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
@@ -56,6 +56,7 @@ export function register(config) {
 }
 
 function registerValidSW(swUrl, config) {
+  console.log('REGISTER URL:', swUrl);
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
